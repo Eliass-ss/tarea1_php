f4a3fe4c12ee8d7ae429ed8cfd789101ff01b53c
Eliass-ss <20236996@clases.edu.sv>
2026-05-18 17:46:18 -0600

    Ejercicio 14
    Se solicita una cantidad (metros) en html y se convierten y muestran en centimetros en php

---
+++ 14_2.php
@@ -0,0 +1,10 @@
+<?php
+$metros = $_POST['metros'];
+$centimetros = ($metros * 100);
+
+echo "<center>";
+echo "Son $centimetros centimetros";
+echo "<br><br>";
+echo "<a href='14_1.html'>Regresar</a>";
+echo "</center>";
+?>
