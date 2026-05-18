6f924bde55ebaa0fd356e72f33411c5152153818
Eliass-ss <20236996@clases.edu.sv>
2026-05-18 17:33:51 -0600

    Ejercicio 4
    Se piden 2 numeros en html, se multiplican y se muestra el resultado en php

---
+++ 4_2.php
@@ -0,0 +1,11 @@
+<?php
+$num1 = $_POST['num1'];
+$num2 = $_POST['num2'];
+$total = ($num1 * $num2);
+
+echo "<center>";
+echo "El resultado de la multiplicacion es:  " . $total;
+echo "<br><br>";
+echo "<a href='4_1.html'>Regresar</a>";
+echo "</center>";
+?>
