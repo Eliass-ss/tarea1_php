580e1e56cf29a971dd0cbea2f6a7fe3edb813af4
Eliass-ss <20236996@clases.edu.sv>
2026-05-18 17:34:39 -0600

    Ejercicio 5
    Se piden 2 numeros en html, se dividen y se muestra el resultado en php

---
+++ 5_2.php
@@ -0,0 +1,11 @@
+<?php
+$num1 = $_POST['num1'];
+$num2 = $_POST['num2'];
+$total = ($num1 / $num2);
+
+echo "<center>";
+echo "El resultado de la division es:  " . $total;
+echo "<br><br>";
+echo "<a href='5_1.html'>Regresar</a>";
+echo "</center>";
+?>
