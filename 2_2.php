7b735dfacf03d014ea8b61bb11c0b9e9121829a2
Eliass-ss <20236996@clases.edu.sv>
2026-05-18 17:31:52 -0600

    Ejercicio 2
    se piden 2 numeros en html, se suman y muestra el resultado en php

---
+++ 2_2.php
@@ -0,0 +1,11 @@
+<?php
+$num1 = $_POST['num1'];
+$num2 = $_POST['num2'];
+$total = ($num1 + $num2);
+
+echo "<center>";
+echo "El resultado de la suma es:  " . $total;
+echo "<br><br>";
+echo "<a href='2_1.html'>Regresar</a>";
+echo "</center>";
+?>
