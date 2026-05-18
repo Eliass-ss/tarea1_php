dea2026bed54434bfaedd634a2d7747c0337bf29
Eliass-ss <20236996@clases.edu.sv>
2026-05-18 17:43:12 -0600

    Ejercicio 12
    Se solicita la base y altura de un rectangulo en html y se calcula y muestra el area en php

---
+++ 12_2.php
@@ -0,0 +1,11 @@
+<?php
+$base = $_POST['base'];
+$altura = $_POST['altura'];
+$area = ($base * $altura);
+
+echo "<center>";
+echo "El area es: " . $area;
+echo "<br><br>";
+echo "<a href='12_1.html'>Regresar</a>";
+echo "</center>";
+?>
