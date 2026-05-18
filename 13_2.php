84d16a8ace7ebfc45d45af4b308c87e125bd7479
Eliass-ss <20236996@clases.edu.sv>
2026-05-18 17:44:28 -0600

    Ejercicio 13
    Se solicita el valor de un lado de un cuadrado y se calcula y muestra el perimetro en php

---
+++ 13_2.php
@@ -0,0 +1,10 @@
+<?php
+$lado = $_POST['lado'];
+$perimetro = ($lado * 4);
+
+echo "<center>";
+echo "El perimetro es: " . $perimetro;
+echo "<br><br>";
+echo "<a href='13_1.html'>Regresar</a>";
+echo "</center>";
+?>
