f32d0c6fb36fe4c704aa4697de24ca7e76bd45fd
Eliass-ss <20236996@clases.edu.sv>
2026-05-18 17:35:52 -0600

    Ejercicio 6
    Se solicita el nombre y edad de un persona en html y se muestran los datos en php

---
+++ 6_2.php
@@ -0,0 +1,12 @@
+<?php
+$nombre = $_POST['nombre'];
+$edad = $_POST['edad'];
+
+echo "<center>";
+echo "Nombre: " . $nombre;
+echo "<br><br>";
+echo "Edad: " . $edad;
+echo "<br><br>";
+echo "<a href='6_1.html'>Regresar</a>";
+echo "</center>";
+?>
