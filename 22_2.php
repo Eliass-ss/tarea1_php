36a222ca2101f24cdd08963e16bdf5c5778426d6
Eliass-ss <20236996@clases.edu.sv>
2026-05-18 22:26:05 -0600

    Ejercicio 22
    Se solicita una cantidad en dolares y se convierte a Euros en php

---
+++ 22_2.php
@@ -0,0 +1,10 @@
+<?php
+$dolar = $_POST['dolar'];
+$euro = ($dolar * 0.88);
+
+echo "<center>";
+echo "$dolar$ = $euro euros";
+echo "<br><br>";
+echo "<a href='22_1.html'>Regresar</a>";
+echo "</center>";
+?>
