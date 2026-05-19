1fbf60d8dbb28f7c08047989c5e93dd57e6fdd8a
Eliass-ss <20236996@clases.edu.sv>
2026-05-18 22:24:55 -0600

    Ejercicio 21
    Se solicita una palabra y luego se muestra cuantas letras tiene esa palabra en php

---
+++ 21_2.php
@@ -0,0 +1,10 @@
+<?php
+$palabra = $_POST['palabra'];
+$cant = strlen($palabra);
+
+echo "<center>";
+echo "La palabra tiene $cant letras";
+echo "<br><br>";
+echo "<a href='21_1.html'>Regresar</a>";
+echo "</center>";
+?>
