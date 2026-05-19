9abe3b7a019870082e843639e0eb491b49d23931
Eliass-ss <20236996@clases.edu.sv>
2026-05-18 22:30:05 -0600

    Ejercicio 25
    Se solicita el nombre, correo y ciudad de un usuario y se muestran los datos ingresados en php

---
+++ 25_2.php
@@ -0,0 +1,15 @@
+<?php
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$ciudad = $_POST['ciudad'];
+
+echo "<center>";
+echo "Nombre: $nombre";
+echo "<br><br>";
+echo "Correo: $correo";
+echo "<br><br>";
+echo "Ciudad: $ciudad";
+echo "<br><br>";
+echo "<a href='25_1.html'>Regresar</a>";
+echo "</center>";
+?>
