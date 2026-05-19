92b9eda803c6f2d3fbdebd2b960bef8d15bbef59
Eliass-ss <20236996@clases.edu.sv>
2026-05-18 22:28:53 -0600

    Ejercicio 24
    Se solicita la edad actual de una persona y se muestra cuanto tendra en 10 años

---
+++ 24_2.php
@@ -0,0 +1,10 @@
+<?php
+$edad = $_POST['edad'];
+$futura = ($edad + 10);
+
+echo "<center>";
+echo "En 10 años tendra $futura años";
+echo "<br><br>";
+echo "<a href='24_1.html'>Regresar</a>";
+echo "</center>";
+?>
